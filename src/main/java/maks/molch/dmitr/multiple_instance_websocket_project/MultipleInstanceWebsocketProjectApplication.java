c59986c243e86b8057268f0191b5e7b1272abbae
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-09-18 23:34:46 +0300

    init

---
+++ src/main/java/maks/molch/dmitr/multiple_instance_websocket_project/MultipleInstanceWebsocketProjectApplication.java
@@ -0,0 +1,13 @@
+package maks.molch.dmitr.multiple_instance_websocket_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleInstanceWebsocketProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleInstanceWebsocketProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maks/molch/dmitr/multiple_instance_websocket_project/MultipleInstanceWebsocketProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package maks.molch.dmitr.multiple_instance_websocket_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleInstanceWebsocketProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
